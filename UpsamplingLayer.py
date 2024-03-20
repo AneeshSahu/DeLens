2f34628f39e70560024c135c5ea572f959d1ca6a
Ansh Dholakia <anshhiro.dholakia@gmail.com>
2024-03-20 01:24:46 -0400

    backward and forwards prop using k matrix mult

--- UpsamplingLayer.py
+++ UpsamplingLayer.py
@@ -8,17 +8,17 @@ class UpsamplingLayer(Layer):
     # Input:  sizeIn, the number of features of data coming in
     # Input:  sizeOut, the number of features for the data coming out.
     # Output:  None
-    def __init__(self, sizeOut):
-        self.sizeOut = sizeOut
+    def __init__(self, factor):
+        self.factor = factor
         super().__init__()
 
     # Input:  dataIn, an NxD data matrix
     # Output:  An NxK data matrix
     def forward(self, dataIn):
         self.setPrevIn(dataIn)
-        output = np.array([[[0.0 for i in range(self.sizeOut)] for j in range(self.sizeOut)] for k in range(dataIn.shape[0])])
+        output = np.array([[[0.0 for i in range(self.factor * dataIn.shape[1])] for j in range(self.factor * dataIn.shape[1])] for k in range(dataIn.shape[0])])
         for layer in range(dataIn.shape[0]):
-            factor = self.sizeOut //  dataIn[layer].shape[0]
+            factor = self.factor
             for i in range(dataIn[layer].shape[0]):
                 for j in range(dataIn[layer].shape[1]):
                     for k in range(i*factor, factor+i*factor):
@@ -31,7 +31,7 @@ class UpsamplingLayer(Layer):
 
     def backward(self, gradIn):
         output = np.array([[[0.0 for i in range(self.getPrevIn().shape[1])] for j in range(self.getPrevIn().shape[2])] for k in range(self.getPrevIn().shape[0])])
-        factor = gradIn.shape[1] // self.getPrevIn().shape[1]
+        factor = self.factor
         for layer in range(self.getPrevIn().shape[0]):
             for i in range(0, len(gradIn[layer]), factor):
                 for j in range(0, len(gradIn[layer]), factor):
